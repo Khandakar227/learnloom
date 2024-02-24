44a0a0d9f3d4d5565bf45b8a39371d99aa261bc7
Khandakar227 <xenonknight54@gmail.com>
2024-02-24 16:17:55 +0600

    Auth with routeguard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Navbar from "@/components/common/Navbar";
 import HeroSection from "@/components/Home/HeroSection";
 
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-  console.log(user, error, isLoading)
   return (
     <div>
       <Navbar/>
