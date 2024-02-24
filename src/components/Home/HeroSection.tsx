e3979b9d655490c0f83578a3bbc2caeaac38527f
Khandakar227 <xenonknight54@gmail.com>
2024-02-24 16:17:42 +0600

    Auth with routeguard

---
+++ src/components/Home/HeroSection.tsx
@@ -0,0 +1,12 @@
+function HeroSection() {
+  return (
+    <section className="max-w-7xl p-4 mx-auto min-h-[95vh] grid md:grid-cols-2 gap-4 justify-center items-center">
+        <div>
+            <h1 className="text-5xl font-bold font-cinzel">Learn Loom</h1>
+            <p className="text-2xl font-semibold">Your Gateway to Comprehensive Education</p>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,29 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  const { user, error, isLoading } = useUser();
+
+  return (
+    <div className="bg-stone-700 shadow">
+      <div className="px-4 py-2 flex justify-between items-center max-w-7xl mx-auto">
+          <Link href={"/"} className="flex gap-1 items-center justify-center">
+              <Image src={"/images/logo-icon.jpg"} width={50} height={50} alt="LearnLoom Logo" className="rounded" />
+              <div className="leading-none grid justify-center items-center font-cinzel font-bold text-2xl">
+                <span>Learn</span>
+                <span>Loom</span>
+              </div>
+          </Link>
+          {
+            isLoading ? <span>Loading..</span>
+            :
+            !user ?
+            <a className="block px-4 py-2 rounded-md bg-purple-950 shadow-sm" href="/api/auth/login">Login</a>
+            :
+            <a className="bg-purple-400 shadow rounded-2xl h-9 w-9 border-purple-950 border" href="/api/auth/me"></a>
+          }
+      </div>
+    </div>
+  )
+}
